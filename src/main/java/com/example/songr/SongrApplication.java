9d6517d4b1089cd163e282b213443dc8f0d90811
A7mad3bd <ahmadabd398@gmail.com>
2022-03-21 23:50:51 +0300

    edit on lab 11 +lab 12

--- src/main/java/com/example/songr/SongrApplication.java
+++ src/main/java/com/example/songr/SongrApplication.java
@@ -2,10 +2,7 @@ package com.example.songr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
-
 @SpringBootApplication
-@RestController
 public class SongrApplication {
 
 	public static void main(String[] args) {
